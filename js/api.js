31d6847a9e0ee638f6a9b6b6b071cae6a78cc37c
Maarten Ronteltap <maartenronteltap@gmail.com>
2017-03-15 08:34:02 +0100

    commit by Maarten: - simplified the JS by adding .ajaxSetup function. Now all we need to do is adjust the URL with each GET request (but I still need to figure out how to adjust it) - clarified the TXT file

--- js/api.js
+++ js/api.js
@@ -1,38 +1,17 @@
 $(document).ready(function() {
-	var settings = {
+	$.ajaxSetup({
 		"async": true,
 		"crossDomain": true,
-		"url": "https://api.themoviedb.org/3/configuration?api_key=9961a998f004227dea7c58371f36ef5f",
 		"method": "GET",
 		"headers": {},
 		"data": "{}"
+	});
+
+	var settings = {
+		"url": "https://api.themoviedb.org/3/discover/movie?api_key=9961a998f004227dea7c58371f36ef5f&language=en-US&sort_by=vote_average.asc&include_adult=true&include_video=false&page=1&vote_count.gte=1000&with_genres=28",
 	};
 
 	$.ajax(settings).done(function(response) {
 		console.log(response);
 	});
-	var settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "https://api.themoviedb.org/3/genre/movie/list?api_key=9961a998f004227dea7c58371f36ef5f",
-		"method": "GET",
-		"headers": {},
-		"data": "{}"
-	};
-
-	$.ajax(settings).done(function(response) {
-		console.log(response);
-	});	
-	var settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "https://api.themoviedb.org/3/discover/movie?api_key=9961a998f004227dea7c58371f36ef5f&language=en-US&sort_by=vote_average.asc&include_adult=true&include_video=false&page=1&vote_count.gte=1000&with_genres=28",
-		"method": "GET",
-		"headers": {},
-		"data": "{}"
-	};
-
-	$.ajax(settings).done(function(response) {
-		console.log(response);
-	});	
 });
