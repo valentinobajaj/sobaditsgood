441a5ee2fd48c01f6cb7a2cb0f1664b19604c400
Maarten Ronteltap <maartenronteltap@gmail.com>
2017-03-17 02:24:31 +0100

    small change to api.js

--- js/api.js
+++ js/api.js
@@ -15,7 +15,6 @@ $(document).ready(function() {
 		str = str.substring(0, str.length - 1);
 		var baseURL = "https://api.themoviedb.org/3/discover/movie?vote_average.lte=3&vote_count.gte=10&page=1&include_video=false&include_adult=true&sort_by=vote_average.asc&language=en-US&api_key=9961a998f004227dea7c58371f36ef5f&with_genres=";
 		var advancedSearchURL = baseURL.concat(str);
-
 		$(document).ajaxSend(function() {
 			$("#advancedsearchresults").empty();
 			$("#advancedsearchresults").append("<h1>Searching...</h1>");
@@ -27,7 +26,7 @@ $(document).ready(function() {
 	});
 });
 
-
+// BACKUP:
 // $(document).ready(function() {
 // 	$.ajaxSetup({
 // 		"async": true,
