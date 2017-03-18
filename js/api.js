180c813d68cf4260d50aaaccb96ac8badd5f2df1
Maarten Ronteltap <maartenronteltap@gmail.com>
2017-03-18 23:18:08 +0100

    latest changes made on Saturday

--- js/api.js
+++ js/api.js
@@ -9,22 +9,43 @@ $(document).ready(function() {
 		"data": "{}"
 	});
 	$("#advancedsearch").change(function() {
-		var str = "";
+		var suffix = "";
+		var genres = "&with_genres=";
 		$(".genres option:selected").each(function() {
-			str += $(this).attr("value") + ",";
+			genres += $(this).attr("value") + ",";
 		});
-		str = str.substring(0, str.length - 1);
-		var baseURL = "https://api.themoviedb.org/3/discover/movie?vote_average.lte=5&vote_count.gte=10&page=1&include_video=false&include_adult=true&sort_by=vote_average.asc&language=en-US&api_key=9961a998f004227dea7c58371f36ef5f&with_genres=";
-		var advancedSearchURL = baseURL.concat(str);
-		console.log(str);
-		$(document).ajaxSend(function() {
-			$("#advancedsearchresults").empty();
-			$("#advancedsearchresults").append("<h1>Searching...</h1>");
-		});
-		$.ajax(advancedSearchURL).done(function(response) {
-			$("#advancedsearchresults").empty();
-			console.log(response)
-		});
+		genres = genres.substring(0, genres.length - 1);
+		var suffix = suffix.concat(genres);
+		console.log("suffix after genres = " + suffix);
+
+		var sort = "&sort_by=";
+
+		var sortValue = function() {
+			var sort = document.forms[0];
+			var radioValue = "";
+			var i;
+			for (i = 0; i < sort.length; i++) {
+				if (sort[i].checked) {
+					radioValue = radioValue + sort[i].value;
+					return value
+				}
+			}
+		};
+		sort = sort.concat(sortValue);
+		suffix = suffix.concat(sort);
+		console.log("suffix after sort = " + suffix);
+
+		// console.log(suffix);
+		// var baseURL = "https://api.themoviedb.org/3/discover/movie?vote_average.lte=5&vote_count.gte=10&page=1&include_video=false&include_adult=true&sort_by=vote_average.asc&language=en-US&api_key=9961a998f004227dea7c58371f36ef5f&with_genres=";
+		// var advancedSearchURL = baseURL.concat(str);		
+		// $(document).ajaxSend(function() {
+		// 	$("#advancedsearchresults").empty();
+		// 	$("#advancedsearchresults").append("<h1>Searching...</h1>");
+		// });
+		// $.ajax(advancedSearchURL).done(function(response) {
+		// 	$("#advancedsearchresults").empty();
+		// 	console.log(response)
+		// });
 	});
 });
 
@@ -43,14 +64,15 @@ $(document).ready(function() {
 // 			str += $(this).attr("value") + ",";
 // 		});
 // 		str = str.substring(0, str.length - 1);
-// 		var baseURL = "https://api.themoviedb.org/3/discover/movie?vote_average.lte=3&vote_count.gte=10&page=1&include_video=false&include_adult=true&sort_by=vote_average.asc&language=en-US&api_key=9961a998f004227dea7c58371f36ef5f&with_genres=";
+// 		var baseURL = "https://api.themoviedb.org/3/discover/movie?vote_average.lte=5&vote_count.gte=10&page=1&include_video=false&include_adult=true&sort_by=vote_average.asc&language=en-US&api_key=9961a998f004227dea7c58371f36ef5f&with_genres=";
 // 		var advancedSearchURL = baseURL.concat(str);
+// 		console.log(str);
 // 		$(document).ajaxSend(function() {
 // 			$("#advancedsearchresults").empty();
 // 			$("#advancedsearchresults").append("<h1>Searching...</h1>");
-// 			console.log(str);
 // 		});
 // 		$.ajax(advancedSearchURL).done(function(response) {
+// 			$("#advancedsearchresults").empty();
 // 			console.log(response)
 // 		});
 // 	});
