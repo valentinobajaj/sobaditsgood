dd010a609011bf3db257f18b1833c61b7d245843
Maarten Ronteltap <maartenronteltap@gmail.com>
2017-03-19 22:38:55 +0100

    basically I have a fully working API now but I'm running into a problem! I will fill you guys in later.

--- js/api.js
+++ js/api.js
@@ -1,21 +1,32 @@
 //coding by Maarten Ronteltap
 
 $(document).ready(function() {
-	$.ajaxSetup({
+	$("#advancedsearch").change(function() {
+		
+		//ThemovieDB.org wants these search settings to always be included
+		var advancedSearchSettings = {
 		"async": true,
 		"crossDomain": true,
 		"method": "GET",
 		"headers": {},
 		"data": "{}"
-	});
-	$("#advancedsearch").change(function() {
+		};
+
+		//set Base URL
+		var baseURL = "https://api.themoviedb.org/3/discover/movie?vote_average.lte=5&vote_average.gte=0.1&vote_count.gte=10&page=1&include_video=false&language=en-US&api_key=9961a998f004227dea7c58371f36ef5f";
+		
+		//create URL suffix...
 		var suffix = "";
+
+		//first for the genres...
 		var genres = "&with_genres=";
 		$(".genres option:selected").each(function() {
 			genres += $(this).attr("value") + ",";
 		});
 		genres = genres.substring(0, genres.length - 1);
 		var suffix = suffix.concat(genres);
+
+		//...then for how the results are to be sorted...
 		var sort = "&sort_by="
 		function sortValue() {
 			var sort = document.querySelectorAll(".sort");
@@ -31,6 +42,8 @@ $(document).ready(function() {
 		var sortValue = sortValue();
 		sort = sort.concat(sortValue);
 		suffix = suffix.concat(sort);
+
+		//...then whether adult content should be included in the search results.
 		var adult = "&include_adult="
 		function adultValue() {
 			var adult = document.querySelectorAll(".adult");
@@ -47,31 +60,36 @@ $(document).ready(function() {
 		adult = adult.concat(adultValue);
 		suffix = suffix.concat(adult);
 		console.log(suffix);
-		var baseURL = "https://api.themoviedb.org/3/discover/movie?vote_average.lte=5&vote_average.gte=0.1&vote_count.gte=10&page=1&include_video=false&language=en-US&api_key=9961a998f004227dea7c58371f36ef5f";
-		var advancedSearchURL = baseURL.concat(suffix);		
-		$(document).ajaxSend(function() {
-			$("#advancedsearchresults").empty();
-			$("#advancedsearchresults").append("<h1>Searching...</h1>");
+
+		// now create a complete URL and include it in the search settings
+		var advancedSearchURL = baseURL.concat(suffix);
+		advancedSearchSettings.URL = advancedSearchURL;
+		console.log(advancedSearchSettings);
+		// $(document).ajaxSend(function() {
+		// 	$("#advancedsearchresults").empty();
+		// 	$("#advancedsearchresults").append("<h1>Searching...</h1>");
+		// });
+		$.ajax(advancedSearchSettings).done(function(response) {
+			// $("#advancedsearchresults").empty();
+			console.log(response)
 		});
-		$.ajax(advancedSearchURL).done(function(response) {
-			$("#advancedsearchresults").empty();
-			console.log(response);
-		});
-		$(document).ajaxError(function() {
-			$("#advancedsearchresults").empty();
-			$("#advancedsearchresults").append("<h1>There was a problem. Please try again later!</h1>");
-		});
-		$(document).ajaxSuccess(function(data){
-			var i;
-			for (i = 0; i < 5; i++) {
-			var image = document.createElement('img');
-			var imageBaseUrl = "https://image.tmdb.org/t/p/w500";
-			var posterPath = data.results[i].poster_path;
-			var imageLocation = imageBaseUrl.concat(posterPath);
-			$(image).attr('src', imageLocation);
-			$('#advancedsearchresults').append(image);
-			console.log(results[i].poster_path)
-			}
-		})
+		// $(document).ajaxSuccess(function(data) {
+		// 	$("#advancedsearchresults").empty();
+		// 	console.log(data);
+		// 	var i;
+		// 	for (i = 0; i < 5; i++) {
+		// 	var image = document.createElement('img');
+		// 	var imageBaseUrl = "https://image.tmdb.org/t/p/w500";
+		// 	var posterPath = data[i].poster_path;
+		// 	var imageLocation = imageBaseUrl.concat(posterPath);
+		// 	$(image).attr('src', imageLocation);
+		// 	$('#advancedsearchresults').append(image);
+		// 	console.log(results[i].poster_path)
+		// 	}
+		// });
+		// $(document).ajaxError(function() {
+		// $("#advancedsearchresults").empty();
+		// $("#advancedsearchresults").append("<h1>There was a problem. Please try again later!</h1>");
+		// });
 	});
 });
