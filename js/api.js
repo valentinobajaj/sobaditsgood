125b7e40e836826f19106c2f8ca4b3820121b466
ValentinoBajaj <valentino.bajaj@gmail.com>
2017-03-20 18:47:55 +0100

    small changes

--- js/api.js
+++ js/api.js
@@ -1,13 +1,25 @@
 //coding by Maarten Ronteltap
 
 $(document).ready(function() {
-	$.ajaxSetup({
-		"async": true,
-		"crossDomain": true,
-		"method": "GET",
-		"headers": {},
-		"data": "{}"
-	});
+	 $("##search-form").submit(function(e) {
+		$.ajaxSetup({
+			url: "https://api.themoviedb.org/3/search/movie",
+			async: true,
+			crossDomain: true,
+			method: "GET",
+			headers: {},
+			data: {
+				api_key: "9961a998f004227dea7c58371f36ef5f"
+				query: "john",
+			}
+			
+
+			success: showImages
+		});
+
+	return false
+	})
+});
 	$("#advancedsearch").change(function() {
 		var str = "";
 		$(".genres option:selected").each(function() {
@@ -26,8 +38,18 @@ $(document).ready(function() {
 			console.log(response)
 		});
 	});
-});
 
+
+function showImages(data) {
+    $("#search-result").html("");
+
+    for (i in data.results) {
+        var image = document.createElement('img');
+        var path = "https://image.tmdb.org/t/p/w500"
+        $(image).attr('src', path.concat(data.results[i].poster_path));
+        $('#search-result').append(image);
+	    }
+	}
 // BACKUP:
 // $(document).ready(function() {
 // 	$.ajaxSetup({
